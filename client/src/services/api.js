d08120b25724e12d3f0ad91cfc690ba787a0a023
tekluabebe <88448375+tekluabebe@users.noreply.github.com>
2026-05-15 20:25:03 +0300

    api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,2 +1,5 @@
 import axios from "axios";
-export default axios.create({ baseURL: "http://localhost:5000/api" });
+
+export default axios.create({ 
+  baseURL: "https://micro-finance-90cq.onrender.com/api" 
+});
